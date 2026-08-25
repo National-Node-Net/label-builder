cf04a672e9651404da85030f5592a2f184283754
Rotimi Joseph <rotimi.joseph@informed.com>
2026-08-25 10:52:03 +0100

    fix: add SPDX headers and remove license variables

--- ianode_labels/ianode_model.py
+++ ianode_labels/ianode_model.py
@@ -1,3 +1,26 @@
+# SPDX-License-Identifier: Apache-2.0
+# Originally developed by Telicent Ltd.; subsequently adapted, enhanced, and maintained by the National Digital Twin Programme.
+
+
+# Copyright (c) Telicent Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Modifications made by the National Digital Twin Programme (NDTP)
+# © Crown Copyright 2026. This work has been developed by the National Digital Twin Programme
+# and is legally attributed to the UK's Department for Business, Innovation, Science and Trade (BIST) as the governing entity.
+
+
 import logging
 from abc import ABC, abstractmethod
 from datetime import datetime, timezone
@@ -8,21 +31,6 @@ from pydantic import AwareDatetime, BaseModel, PlainSerializer, field_validator
 from ianode_labels.security_labels import SecurityLabelBuilder
 from ianode_labels.ianodev2 import IANodeSecurityLabelsV2
 
-__license__ = """
-Copyright (c) Telicent Ltd.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
 
 log = logging.getLogger(__name__)
 
